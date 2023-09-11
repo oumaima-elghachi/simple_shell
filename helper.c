45e9449b2efffbf72f0bd9db58f2edb627f732f9
Samiraaitboubker <samiraaitboubker9@gmail.com>
2023-09-11 16:42:29 -0700

    helper.c

--- helper.c
+++ helper.c
@@ -1,3 +1,10 @@
 /*
  * File: helper.c
  */
+
+#include "shell.h"
+
+void free_args(char **args, char **front);
+char *get_pid(void);
+char *get_env_value(char *beginning, int len);
+void variable_replacement(char **args, int *exe_ret);
