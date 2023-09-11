d488f27b88f8b55c9aeb0048aa92aac766313c39
Samiraaitboubker <samiraaitboubker9@gmail.com>
2023-09-11 16:44:51 -0700

    helper.c

--- helper.c
+++ helper.c
@@ -8,3 +8,9 @@ void free_args(char **args, char **front);
 char *get_pid(void);
 char *get_env_value(char *beginning, int len);
 void variable_replacement(char **args, int *exe_ret);
+
+/**
+ * free_args - Frees up memory taken by args.
+ * @args: A null-terminated double pointer containing commands/arguments.
+ * @front: A double pointer to the beginning of args.
+ */
