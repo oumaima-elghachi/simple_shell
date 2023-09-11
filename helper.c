f45350fd806e416e457b2ceedd4c32fed119e71e
Samiraaitboubker <samiraaitboubker9@gmail.com>
2023-09-11 16:51:52 -0700

    helper.c

--- helper.c
+++ helper.c
@@ -14,3 +14,31 @@ void variable_replacement(char **args, int *exe_ret);
  * @args: A null-terminated double pointer containing commands/arguments.
  * @front: A double pointer to the beginning of args.
  */
+void free_args(char **args, char **front)
+{
+	size_t i;
+
+	for (i = 0; args[i] || args[i + 1]; i++)
+		free(args[i]);
+
+	free(front);
+}
+
+/**
+ * get_pid - Gets the current process ID.
+ * Description: Opens the stat file, a space-delimited file containing
+ *              information about the current process. The PID is the
+ *              first word in the file. The function reads the PID into
+ *              a buffer and replace the space at the end with a \0 byte.
+ *
+ * Return: The current process ID or NULL on failure.
+ */
+char *get_pid(void)
+{
+	size_t i = 0;
+	char *buffer;
+	ssize_t file;
+
+	file = open("/proc/self/stat", O_RDONLY);
+	if (file == -1)
+	{
