0657ec76e2f6d1d3c85f0bba5fcc044dc0ee9d5a
Samiraaitboubker <samiraaitboubker9@gmail.com>
2023-09-11 16:55:18 -0700

    helper.c

--- helper.c
+++ helper.c
@@ -42,3 +42,20 @@ char *get_pid(void)
 	file = open("/proc/self/stat", O_RDONLY);
 	if (file == -1)
 	{
+	        perror("Cant read file");
+		return (NULL);
+	}
+	buffer = malloc(120);
+	if (!buffer)
+	{
+		close(file);
+		return (NULL);
+	}
+	read(file, buffer, 120);
+	while (buffer[i] != ' ')
+		i++;
+	buffer[i] = '\0';
+
+	close(file);
+	return (buffer);
+}
