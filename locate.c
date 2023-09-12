ec99a0529a5a96848a5f3de4fefd1ae14a77ae99
Samiraaitboubker <samiraaitboubker9@gmail.com>
2023-09-11 17:56:34 -0700

    locate.c

--- locate.c
+++ locate.c
@@ -121,3 +121,25 @@ list_t *get_path_dir(char *path)
 	char **dirs, *path_copy;
 	list_t *head = NULL;
 
+	path_copy = fill_path_dir(path);
+	if (!path_copy)
+		return (NULL);
+	dirs = _strtok(path_copy, ":");
+	free(path_copy);
+	if (!dirs)
+		return (NULL);
+
+	for (index = 0; dirs[index]; index++)
+	{
+		if (add_node_end(&head, dirs[index]) == NULL)
+		{
+			free_list(head);
+			free(dirs);
+			return (NULL);
+		}
+	}
+
+	free(dirs);
+
+	return (head);
+}
