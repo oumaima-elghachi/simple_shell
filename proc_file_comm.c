b5e458b8d6853ece13a74285d19d52b7fd46851d
Samiraaitboubker <samiraaitboubker9@gmail.com>
2023-09-11 18:03:55 -0700

    proc_file_comm.c

--- proc_file_comm.c
+++ proc_file_comm.c
@@ -66,4 +66,59 @@ int proc_file_commands(char *file_path, int *exe_ret)
 	{
 		*exe_ret = cant_open(file_path);
 		return (*exe_ret);
+        }
+	line = malloc(sizeof(char) * old_size);
+	if (!line)
+		return (-1);
+	do {
+		b_read = read(file, buffer, 119);
+		if (b_read == 0 && line_size == 0)
+			return (*exe_ret);
+		buffer[b_read] = '\0';
+		line_size += b_read;
+		line = _realloc(line, old_size, line_size);
+		_strcat(line, buffer);
+		old_size = line_size;
+	} while (b_read);
+	for (i = 0; line[i] == '\n'; i++)
+		line[i] = ' ';
+	for (; i < line_size; i++)
+	{
+		if (line[i] == '\n')
+		{
+			line[i] = ';';
+			for (i += 1; i < line_size && line[i] == '\n'; i++)
+				line[i] = ' ';
+		}
 	}
+	variable_replacement(&line, exe_ret);
+	handle_line(&line, line_size);
+	args = _strtok(line, " ");
+	free(line);
+	if (!args)
+		return (0);
+	if (check_args(args) != 0)
+	{
+		*exe_ret = 2;
+		free_args(args, args);
+		return (*exe_ret);
+	}
+	front = args;
+
+	for (i = 0; args[i]; i++)
+	{
+		if (_strncmp(args[i], ";", 1) == 0)
+		{
+			free(args[i]);
+			args[i] = NULL;
+			ret = call_args(args, front, exe_ret);
+			args = &args[++i];
+			i = 0;
+		}
+	}
+
+	ret = call_args(args, front, exe_ret);
+
+	free(front);
+	return (ret);
+}
