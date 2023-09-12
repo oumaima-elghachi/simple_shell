eeecada49ff00d51ac3c50be0ff3ab33a177630d
Samiraaitboubker <samiraaitboubker9@gmail.com>
2023-09-11 17:12:56 -0700

    helpers_2.c

---
+++ helpers_2.c
@@ -0,0 +1,12 @@
+/*
+ * File: helpers_2.c
+ *
+ */
+
+#include "shell.h"
+
+void handle_line(char **line, ssize_t read);
+ssize_t get_new_len(char *line);
+void logical_ops(char *line, ssize_t *new_len);
+
+/**
