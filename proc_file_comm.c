cc5c0da9baaa4588a4a63dff60634c82cfba358c
Samiraaitboubker <samiraaitboubker9@gmail.com>
2023-09-11 18:01:31 -0700

    proc_file_comm.c

--- proc_file_comm.c
+++ proc_file_comm.c
@@ -13,3 +13,57 @@ int proc_file_commands(char *file_path, int *exe_ret);
 
 int cant_open(char *file_path)
 {
+	char *error, *hist_str;
+	int len;
+
+	hist_str = _itoa(hist);
+	if (!hist_str)
+		return (127);
+
+	len = _strlen(name) + _strlen(hist_str) + _strlen(file_path) + 16;
+	error = malloc(sizeof(char) * (len + 1));
+	if (!error)
+	{
+		free(hist_str);
+		return (127);
+	}
+
+	_strcpy(error, name);
+	_strcat(error, ": ");
+	_strcat(error, hist_str);
+	_strcat(error, ": Can't open ");
+	_strcat(error, file_path);
+	_strcat(error, "\n");
+
+	free(hist_str);
+	write(STDERR_FILENO, error, len);
+	free(error);
+	return (127);
+}
+
+/**
+ * proc_file_commands - Takes a file and attempts to run the commands stored
+ * within.
+ * @file_path: Path to the file.
+ * @exe_ret: Return value of the last executed command.
+ *
+ * Return: If file couldn't be opened - 127.
+ *	   If malloc fails - -1.
+ *	   Otherwise the return value of the last command ran.
+ */
+int proc_file_commands(char *file_path, int *exe_ret)
+{
+	ssize_t file, b_read, i;
+	unsigned int line_size = 0;
+	unsigned int old_size = 120;
+	char *line, **args, **front;
+	char buffer[120];
+	int ret;
+
+	hist = 0;
+	file = open(file_path, O_RDONLY);
+	if (file == -1)
+	{
+		*exe_ret = cant_open(file_path);
+		return (*exe_ret);
+	}
