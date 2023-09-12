3b72120c7ccf07927eafd2827687d8a192493e40
Samiraaitboubker <samiraaitboubker9@gmail.com>
2023-09-11 17:31:35 -0700

    input_helpers.c

---
+++ input_helpers.c
@@ -0,0 +1,23 @@
+/*
+ * File: input_helpers.c
+ *
+ */
+
+#include "shell.h"
+
+char *get_args(char *line, int *exe_ret);
+int call_args(char **args, char **front, int *exe_ret);
+int run_args(char **args, char **front, int *exe_ret);
+int handle_args(int *exe_ret);
+int check_args(char **args);
+
+/**
+ * get_args - Gets a command from standard input.
+ * @line: A buffer to store the command.
+ * @exe_ret: The return value of the last executed command.
+ *
+ * Return: If an error occurs - NULL.
+ *         Otherwise - a pointer to the stored command.
+ */
+char *get_args(char *line, int *exe_ret)
+{
