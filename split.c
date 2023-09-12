31d73d18e7e8d8416683e408842264454269c3c7
Samiraaitboubker <samiraaitboubker9@gmail.com>
2023-09-11 18:08:47 -0700

    split.c

---
+++ split.c
@@ -0,0 +1,32 @@
+/*
+ * File: split.c
+ *
+ */
+
+#include "shell.h"
+
+int token_len(char *str, char *delim);
+int count_tokens(char *str, char *delim);
+char **_strtok(char *line, char *delim);
+
+/**
+ * token_len - Locates the delimiter index marking the end
+ *             of the first token contained within a string.
+ * @str: The string to be searched.
+ * @delim: The delimiter character.
+ *
+ * Return: The delimiter index marking the end of
+ *         the intitial token pointed to be str.
+ */
+int token_len(char *str, char *delim)
+{
+	int index = 0, len = 0;
+
+	while (*(str + index) && *(str + index) != *delim)
+	{
+		len++;
+		index++;
+	}
+
+	return (len);
+}
