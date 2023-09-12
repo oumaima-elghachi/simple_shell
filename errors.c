a8c13a029127534f78ac676aeef285dd93d14089
oumaima <elghachiomaima1@gmail.com>
2023-09-12 11:53:01 +0100

    update3

--- errors.c
+++ errors.c
@@ -37,40 +37,4 @@ int num_len(int num)
 	return (len);
 }
 
-/**
- * _itoa - Converts an integer to a string.
- * @num: The integer.
- *
- * Return: The converted string.
- */
-char *_itoa(int num)
-{
-	char *buffer;
-	int len = num_len(num);
-	unsigned int num1;
 
-	buffer = malloc(sizeof(char) * (len + 1));
-	if (!buffer)
-		return (NULL);
-
-	buffer[len] = '\0';
-
-	if (num < 0)
-	{
-		num1 = num * -1;
-		buffer[0] = '-';
-	}
-	else
-	{
-		num1 = num;
-	}
-
-	len--;
-	do {
-		buffer[len] = (num1 % 10) + '0';
-		num1 /= 10;
-		len--;
-	} while (num1 > 0);
-
-	return (buffer);
-}
