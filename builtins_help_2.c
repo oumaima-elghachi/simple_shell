52fb99db60908d182bdd9babf6a9c57310864681
OUMAIMA EL GHACHI <128837415+oumaima-elghachi@users.noreply.github.com>
2023-09-18 09:01:39 +0100

    Update builtins_help_2.c
    updt4

--- builtins_help_2.c
+++ builtins_help_2.c
@@ -6,6 +6,16 @@ void help_unsetenv(void);
 void help_history(void);
 
 /**
+ * help_env - Displays information on the shellby builtin command 'env'.
+ */
+void help_env(void)
+{
+	char *msg = "env: env\n\tPrints the current environment.\n";
+
+	write(STDOUT_FILENO, msg, _strlen(msg));
+}
+
+/**
  * help_unsetenv - Displays information on the shellby builtin command
  * 'unsetenv'.
  */
