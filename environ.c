c136912cae049981d7f778a355b4e19e8190b0ae
OUMAIMA EL GHACHI <128837415+oumaima-elghachi@users.noreply.github.com>
2023-09-19 15:56:14 +0100

    Update environ.c
    update9

--- environ.c
+++ environ.c
@@ -1,8 +1,5 @@
-
 /*
  * File: environ.c
- *
- *
  */
 
 #include "shell.h"
@@ -58,4 +55,25 @@ void free_env(void)
 	for (index = 0; environ[index]; index++)
 		free(environ[index]);
 	free(environ);
+}
+
+/**
+ * _getenv - Gets an environmental variable from the PATH.
+ * @var: The name of the environmental variable to get.
+ *
+ * Return: If the environmental variable does not exist - NULL.
+ *         Otherwise - a pointer to the environmental variable.
+ */
+char **_getenv(const char *var)
+{
+	int index, len;
+
+	len = _strlen(var);
+	for (index = 0; environ[index]; index++)
+	{
+		if (_strncmp(var, environ[index], len) == 0)
+			return (&environ[index]);
+	}
+
+	return (NULL);
 }
