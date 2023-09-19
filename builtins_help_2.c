3159759c4ec5787a1185a075a4f1351b69057596
OUMAIMA EL GHACHI <128837415+oumaima-elghachi@users.noreply.github.com>
2023-09-19 10:04:55 +0100

    Update builtins_help_2.c
    update8

--- builtins_help_2.c
+++ builtins_help_2.c
@@ -15,6 +15,22 @@ void help_env(void)
 	write(STDOUT_FILENO, msg, _strlen(msg));
 }
 
+
+/**
+ * help_setenv - Displays information on the shellby builtin command 'setenv'.
+ */
+void help_setenv(void)
+{
+	char *msg = "setenv: setenv [VARIABLE] [VALUE]\n\tInitializes a new";
+
+	write(STDOUT_FILENO, msg, _strlen(msg));
+	msg = "environment variable, or modifies an existing one.\n\n";
+	write(STDOUT_FILENO, msg, _strlen(msg));
+	msg = "\tUpon failure, prints a message to stderr.\n";
+	write(STDOUT_FILENO, msg, _strlen(msg));
+}
+
+
 /**
  * help_unsetenv - Displays information on the shellby builtin command
  * 'unsetenv'.
