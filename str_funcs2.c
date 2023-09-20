c71f69aec354eae608eaeda3090b2d2f7de00a96
oumaima <elghachiomaima1@gmail.com>
2023-09-20 21:14:55 +0100

    str-func2 updated

--- str_funcs2.c
+++ str_funcs2.c
@@ -60,6 +60,29 @@ int _strspn(char *s, char *accept)
 }
 
 /**
+ * _strcmp - Compares two strings.
+ * @s1: The first string to be compared.
+ * @s2: The second string to be compared.
+ *
+ * Return: Positive byte difference if s1 > s2
+ *         0 if s1 = s2
+ *         Negative byte difference if s1 < s2
+ */
+int _strcmp(char *s1, char *s2)
+{
+	while (*s1 && *s2 && *s1 == *s2)
+	{
+		s1++;
+		s2++;
+	}
+
+	if (*s1 != *s2)
+		return (*s1 - *s2);
+
+	return (0);
+}
+
+/**
  * _strncmp - Compare two strings.
  * @s1: Pointer to a string.
  * @s2: Pointer to a string.
